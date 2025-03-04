b22e359b1ed18c52adde2c1dc1f272497bf175d2
macjjuni <macjjuni@strato.co.kr>
2025-03-04 16:51:22 +0900

    feature. 소개 페이지 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from '../public/react.svg';
+import dockerLogo from '../public/docker.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Hello World {count}!
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <div className="logo__aera">
+                <div className="logo__aera__logo">
+                    {reactLogo && <img src={reactLogo as string} className="logo react" alt="React logo"/>}
+                </div>
+                <div className="logo__aera__logo">
+                    {reactLogo && <img src={dockerLogo as string} className="logo react" alt="React logo"/>}
+                </div>
+            </div>
+            <h1>React & Docker</h1>
+            <div className="card">
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+        </>
+    );
 }
 
-export default App
+export default App;
